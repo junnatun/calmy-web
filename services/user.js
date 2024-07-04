3c1e70559b699f9bea96d660d7740e98beb5ee6e
junnatun <junsa02@gmail.com>
2024-07-04 23:53:06 +0700

    feat(dashboard): integrate get user data

--- services/user.js
+++ services/user.js
@@ -1,4 +1,5 @@
 import { auth } from "./firebase_config.js";
+import { readDoc } from "./firestore.js"
 
 export const getUser = () => {
     return new Promise((resolve, reject) => {
@@ -18,3 +19,38 @@ export const getUser = () => {
         });
     });
 };
+
+const getUserData = async () => {
+    // Get user data
+    const userResponse = await getUser();
+
+    if (!userResponse.status) {
+        throw new Error("User is not signed in");
+    }
+
+    // Get user's email
+    const email = userResponse.data.email;
+
+    // Fetch user document from the database using email
+    const userDoc = await readDoc("users", email);
+
+    // Get the username from the user document
+    const username = userDoc.data().username;
+    console.log(username);
+
+    const usernameContainer = document.getElementById('username');
+
+    // Clear the container (optional, if you want to start fresh each time)
+    usernameContainer.innerHTML = '';
+
+    const titleElement = document.createElement('p');
+    titleElement.className = "text-3xl font-bold mt-4";
+    titleElement.textContent = username;
+
+    usernameContainer.appendChild(titleElement);
+}
+
+// Call the function when the document is fully loaded
+window.addEventListener('DOMContentLoaded', () => {
+    getUserData();
+});
